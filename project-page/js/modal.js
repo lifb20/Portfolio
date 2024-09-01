b253b08ae142d03f1ab3bd5d628cf4d088fb978d
Liam <lifbrowne@gmail.com>
2024-09-01 16:06:04 +0100

    Added landing & projects page

---
+++ project-page/js/modal.js
@@ -0,0 +1,13 @@
+const open_modal = document.getElementById('open_modal');
+const modal_container = document.getElementById('modal_container');
+const close_modal = document.getElementById('close_modal');
+
+open_modal.addEventListener('click', () => {
+    open_modal.innerHTML = "worked";
+    modal_container.classList.add('show');
+});
+
+
+close_modal.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
