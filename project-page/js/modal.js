5e80e137f978b011b9dcb8177f6c300948c2139f
Liam <lifbrowne@gmail.com>
2024-09-02 12:26:44 +0100

    Page transition complete

--- project-page/js/modal.js
+++ project-page/js/modal.js
@@ -1,6 +1,6 @@
-const open_modal = document.getElementById('open_modal');
+const open_modal = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
-const close_modal = document.getElementById('close_modal');
+const close_modal = document.getElementById('close');
 
 open_modal.addEventListener('click', () => {
     modal_container.classList.add('show');
