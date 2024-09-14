034107236a2e0c8367da65ae405f99e06c495e00
Liam <lifbrowne@gmail.com>
2024-09-14 22:07:39 +0100

    More projects added

--- js/content.js
+++ js/content.js
@@ -89,7 +89,7 @@ const project1 = {
     "img-source": 'img/id_1.png',
 
     // Modal content
-    "setting": "Setting: Imperial College Coursework",
+    "setting": "Setting: Imperial College London",
     "date": "Date: 01/21–02/21",
     "type": "Type: Individual",
 
@@ -122,23 +122,61 @@ const project2 = {
     "id": "2",
 
     // Box content
-    "title": "CPU Design Project",
-    "tags": ["ALL", "FEATURED", "DD"],
-    "img-source": 'img/id_1.png',
+    "title": "Hardware Acceleration on FPGA for NIOS II Processor",
+    "tags": ["ALL", "FEATURED", "FPGA", "VERILOG", "MATLAB", "C"],
+    "img-source": 'img/id_2.png',
 
     // Modal content
-    "setting": "Setting: Imperial College Coursework",
-    "date": "Date: 01/21–02/21",
+    "setting": "Setting: Imperial College London",
+    "date": "Date: 01/24–03/24",
     "type": "Type: Individual",
 
     "gitHub": false,
     "gitHub-link": "Report-Link",
     "report": true,
-    "report-link": "documents/id_1.pdf",
+    "report-link": "documents/id_2.pdf",
     "website": false,
     "website-link": "Website-Link",
 
-    "img-src": "img/id_1.png",
+    "img-src": "img/id_2.png",
+
+    "description": ["Hello this is my first project", " lorem ipsum two", "lorem ipsum three", "five"],
+
+    "info-list-one-toggle": false,
+    "info-list-one-title": 'Info Title one',
+    "info-list-one-items": ["item1", "item2", "item3"],
+
+    "info-list-two-toggle": false,
+    "info-list-two-title": 'Info Title two',
+    "info-list-two-items": ["item1", "item2", "item3"],
+
+    "info-list-three-toggle": false,
+    "info-list-three-title": 'Info Title three',
+    "info-list-three-items": ["item1", "item2", "item3"],
+}
+
+const project3 = {
+    // Unique project ID
+    "id": "3",
+
+    // Box content
+    "title": "Portfolio Website",
+    "tags": ["ALL", "FEATURED", "JS", "FULLS", "FRONTEND"],
+    "img-source": 'img/id_3.png',
+
+    // Modal content
+    "setting": "Setting: Personal Project",
+    "date": "Date: 09/24",
+    "type": "Type: Individual",
+
+    "gitHub": true,
+    "gitHub-link": "https://github.com/lifb20/Portfolio",
+    "report": false,
+    "report-link": "documents/id_2.pdf",
+    "website": true,
+    "website-link": "https://lifb20.github.io/Portfolio/",
+
+    "img-src": "img/id_3.png",
 
     "description": ["Hello this is my first project", " lorem ipsum two", "lorem ipsum three", "five"],
 
@@ -156,7 +194,8 @@ const project2 = {
 }
 
 
-const content = [project1, project2];
+
+const content = [project1, project2, project3];
 
 const contentObjects = content.map( contentItem => 
     new Project(contentItem)
